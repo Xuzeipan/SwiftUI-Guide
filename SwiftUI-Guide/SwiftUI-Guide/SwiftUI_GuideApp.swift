a0577e28528b1e798981c535d426d3d2809db5bb
Xuzeipan <1435157300@qq.com>
2023-04-19 10:03:17 +0800

    初始化项目

---
+++ SwiftUI-Guide/SwiftUI-Guide/SwiftUI_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GuideApp.swift
+//  SwiftUI-Guide
+//
+//  Created by xuzeipan on 2023/4/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
